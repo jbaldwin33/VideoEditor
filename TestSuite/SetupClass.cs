fe07eb152cbb7ae1768fb05b56702dea533436a5
Joshua Baldwin <jbald9312@gmail.com>
2022-02-27 10:15:17 -0500

    disable shadow copying in unit tests

--- TestSuite/SetupClass.cs
+++ TestSuite/SetupClass.cs
@@ -2,26 +2,47 @@
 using Moq.Protected;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VideoEditorUi.Utilities;
+using VideoEditorUi.ViewModels;
+using VideoUtilities;
 
 namespace TestSuite
 {
-    public interface IProtectedMembers
-    {
-        IUtilityClass UtilityClass { get; set; }
-    }
-
     public static class SetupClass
     {
-        public static T CreateSut<T>() where T : VideoEditorUi.ViewModels.EditorViewModel
+        public static void SplitterSetup(SplitterViewModel sut)
         {
-            var sut = new Mock<T>();
-            
-            //sut.UtilityClass = new FakeUtilityClass();
-            return sut.Object;
+            sut.CanCombine = false;
+            sut.StartTimeSet = false;
+            sut.StartTime = sut.EndTime = TimeSpan.FromMilliseconds(0);
+            sut.CurrentTimeString = "00:00:00:000";
+            sut.PositionChanged = time => sut.CurrentTimeString = time.ToString("hh':'mm':'ss':'fff");
+            sut.SectionViewModels = new ObservableCollection<SectionViewModel>();
+            sut.RectCollection = new ObservableCollection<RectClass>();
+            sut.Formats = FormatTypeViewModel.CreateViewModels();
+            sut.GetPlayerPosition = () => sut.StartTime.Add(new TimeSpan(0, 0, 5));
+            sut.AddRectangleEvent = () => sut.RectCollection.Add(new RectClass());
+        }
+
+        public static void ChapterAdderSetup(ChapterAdderViewModel sut)
+        {
+            sut.StartTimeSet = false;
+            sut.StartTime = sut.EndTime = TimeSpan.FromMilliseconds(0);
+            sut.CurrentTimeString = "00:00:00:000";
+            sut.PositionChanged = time => sut.CurrentTimeString = time.ToString("hh':'mm':'ss':'fff");
+            sut.SectionViewModels = new ObservableCollection<SectionViewModel>();
+            sut.RectCollection = new ObservableCollection<RectClass>();
+            sut.GetPlayerPosition = () => sut.StartTime.Add(new TimeSpan(0, 0, 5));
+        }
+
+        public static void ChapterAdderSetup(SpeedChangerViewModel sut)
+        {
+            sut.WithSlider = false;
+            sut.FlipScale = 1;
         }
     }
 }
--- TestSuite/SplitterTests.cs
+++ TestSuite/SplitterTests.cs
@@ -9,26 +9,14 @@ namespace TestSuite
 
     public class SplitterTests
     {
-        private void Setup(SplitterViewModel sut)
-        {
-            sut.CanCombine = false;
-            sut.StartTimeSet = false;
-            sut.StartTime = sut.EndTime = TimeSpan.FromMilliseconds(0);
-            sut.CurrentTimeString = "00:00:00:000";
-            sut.PositionChanged = time => sut.CurrentTimeString = time.ToString("hh':'mm':'ss':'fff");
-            sut.SectionViewModels = new ObservableCollection<SectionViewModel>();
-            sut.RectCollection = new ObservableCollection<RectClass>();
-            sut.Formats = FormatTypeViewModel.CreateViewModels();
-            sut.GetPlayerPosition = () => sut.StartTime.Add(new TimeSpan(0, 0, 5));
-            sut.AddRectangleEvent = () => sut.RectCollection.Add(new RectClass());
-        }
+        
         [Theory]
         [InlineData(false, 0)]
         [InlineData(true, 2)]
         public void Verify_Collection_After_Finish(bool isError, int collectionCount)
         {
             var sut = new SplitterViewModel(new FakeUtilityClass(), new FakeEditorService());
-            Setup(sut);
+            SetupClass.SplitterSetup(sut);
             sut.StartCommand.Execute(null);
             sut.EndCommand.Execute(null);
             sut.StartCommand.Execute(null);
@@ -40,5 +28,11 @@ namespace TestSuite
             Assert.Equal(collectionCount,  sut.RectCollection.Count);
         }
 
+        [Fact]
+        public void Verify_Format_On_Load()
+        {
+            var sut = new SplitterViewModel(new FakeUtilityClass(), new FakeEditorService());
+            SetupClass.SplitterSetup(sut);
+        }
     }
 }
